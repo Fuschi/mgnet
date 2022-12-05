6f476d0f8cd4967aaaa29c4e6e06c0329f5c869f
Alessandro Fuschi <alessandro.fuschi2@unibo.it>
2022-12-05 16:06:05 +0100

    add mgnet class part 2 (only grammatical fix)

--- R/class-mgnet.R
+++ R/class-mgnet.R
@@ -40,21 +40,21 @@ mgnet <- setClass(
     # CHECK NETW
     #-------------------------------------#
     if(length(object@netw)!=0){
-      if(is_directed(obejct@netw)) return("\n netw slot must be an undirected igraph object")
+      if(is_directed(object@netw)) return("\n netw slot must be an undirected igraph object")
       if(!is_weighted(object@netw)) return("\n netw slot must be an weighted igraph object")
     }
     
     # CHECK NETW AND DATA
     #-------------------------------------#
     if(length(object@netw)!=0 & length(object@data)!=0){
-      if(vcount(object@netw)!=ncol(obejct@data)) return("\n netw vertices number must be equal to columns number of data")
+      if(vcount(object@netw)!=ncol(object@data)) return("\n netw vertices number must be equal to columns number of data")
       if(any(colnames(object@data)!=V(object@netw)$name)) return("\n netw vertex name and colnames of data must be equal")
     }
     
     # CHECK NETW AND TAXA
     #-------------------------------------#
     if(length(object@netw)!=0 & length(object@taxa)!=0){
-      if(vcount(object@netw)!=nrow(obejct@taxa)) return("\n netw vertices number must be equal to rows number of taxa")
+      if(vcount(object@netw)!=nrow(object@taxa)) return("\n netw vertices number must be equal to rows number of taxa")
       if(any(rownames(object@taxa)!=V(object@netw)$name)) return("\n netw vertex name and rownames of taxa must be equal")
     }
     
@@ -62,8 +62,9 @@ mgnet <- setClass(
     #-------------------------------------#
     if(length(object@comm)!=0 & length(object@netw)==0) return("\n you can't have the comm slot without its associated netw")
     if(length(object@comm)!=0){
-      if(object@comm$vcount!=vcount(obejct@netw)) stop("comm and netw must have the same number of vertices")
+      if(object@comm$vcount!=vcount(object@netw)) stop("comm and netw must have the same number of vertices")
     }
     
+    TRUE
   }
 )
